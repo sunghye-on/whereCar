2dd732104a15d5a75b87c1864e58edeb30248f02
gorilla-Kim <00ghks22@naver.com>
2020-02-09 01:56:04 +0900

    create LoginBotton component

--- FRONT-END/src/lib/api/auth.js
+++ FRONT-END/src/lib/api/auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 
-export const checkEmailExists = (email) => axios.get(`/api/auth/v1.0/exists/email/` + email);
-export const checkUsernameExists = (username) => axios.get(`/api/auth/v1.0/exists/username/` + username);
+export const checkEmailExists = (email) => axios.get(`/api/v1.0/auth/exists/email/` + email);
+export const checkUsernameExists = (username) => axios.get(`/api/v1.0/auth/exists/username/` + username);
 
-export const localRegister = ({email, username, password}) => axios.post(`/api/auth/v1.0/register/local`, { email, username, password });
-export const localLogin = ({email, password}) => axios.post(`/api/auth/v1.0/login/local`, { email, password });
+export const localRegister = ({email, username, password}) => axios.post(`/api/v1.0/auth/register/local`, { email, username, password });
+export const localLogin = ({email, password}) => axios.post(`/api/v1.0/auth/login/local`, { email, password });
 
 export const checkStatus = () => axios.get(`/api/v1.0/auth/check`);
-export const logout = () => axios.post(`/api/auth/v1.0/logout`);
+export const logout = () => axios.post(`/api/v1.0/auth/logout`);
