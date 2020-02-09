42361e7fec331f69be70fd7e59e98377ddd8b7ab
gorilla-Kim <00ghks22@naver.com>
2020-02-09 22:46:57 +0900

    hiding header components 특정 사이트에서 헤더가 숨겨지는 기능 구현 redux 사용
    react의 components들은 일반적으로 redux에 rendering 되어질수 없다. 그렇기 때문에 react-redux 에서 제공하는 connect라는 함수를 이용해 rendering이 이루어져야 한다.
    
    redux-actions
    redux는 reducer 기반이다. 그렇기 때문에 특정 Type에 해당하는 요청을 만드는 Action 함수가 필수적이다. 이러한 Action함수를 좀 더 쉽고 간단하고 유용하게 만들어주는 함수이다.
    
    createAction
    redux-actions 에서 action을 만들어주는 함수이다. 간단하게 파라미터로 type만 넣어주면 된다.
    
    handleAction
    reducer에 해당하는 부분이다. handleAction안에 객체형태로 (Key:value) 작성이 이루어진다. reducer와 흡사하게 state와 action을 이용해 특정 state값을 변경한다.
    
    bindActionCreator
    특정 Action을 만들어주는 ActionCreator를 특정 변수에 binding 해주는 함수이다.

--- FRONT-END/src/containers/Base/HeaderContainer.js
+++ FRONT-END/src/containers/Base/HeaderContainer.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import Header from 'components/Base/Header';
+import Header, { LoginButton } from 'components/Base/Header';
+import { connect } from 'react-redux';
 
-export default function HeaderContainer() {
+function HeaderContainer({ visible }) {
+  if(!visible) return null;
+
   return (
-    <Header>
-        Hi
-    </Header>
+      <Header>
+          <LoginButton/>
+      </Header>
   );
 };
+
+export default connect(
+  (state) => ({
+      visible: state.base.getIn(['header', 'visible'])
+  }),
+  (dispatch) => ({
+
+  })
+)(HeaderContainer);
--- FRONT-END/src/pages/Auth.js
+++ FRONT-END/src/pages/Auth.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as baseActions from 'redux/modules/base';
 
-export default function Auth() {
+function Auth({ BaseActions }) {
+  useEffect(() => {
+    BaseActions.setHeaderVisibility(false);
+    return () => {
+      BaseActions.setHeaderVisibility(true);
+    };
+  }, [BaseActions]);
+
   return (
     <div>
         Auth
     </div>
   );
-}
+};
+
+export default connect(
+  (state) => ({
+
+  }),
+  (dispatch) => ({
+      BaseActions: bindActionCreators(baseActions, dispatch)
+  })
+)(Auth);
