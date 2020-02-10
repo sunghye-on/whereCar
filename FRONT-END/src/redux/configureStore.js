c2523979da791075d798320b7603c5ecb3aa4949
gorilla-Kim <00ghks22@naver.com>
2020-02-10 14:08:41 +0900

    apply redux-pender

--- FRONT-END/src/redux/configureStore.js
+++ FRONT-END/src/redux/configureStore.js
@@ -1,12 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import penderMiddleware from 'redux-pender';
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension'; 
 import ReduxThunk from 'redux-thunk';   
 import modules from './modules';
 
 // Store를 생성해서 반환하는 fuction 입니다.
 const configureStore = (initialState) => {
-    const store = createStore(modules, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
+    const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+    const composeEnhancers = devTools || compose;
+
+    const middlewares = [ReduxThunk, penderMiddleware(), logger];
+
+    const store = createStore(modules, composeEnhancers(
+        applyMiddleware(...middlewares)
+    ));
     return store;
 }
 
--- FRONT-END/src/redux/modules/index.js
+++ FRONT-END/src/redux/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import base from './base';
 import auth from './auth';
+import { penderReducer } from 'redux-pender';
 
 // reducer들을 하나로 병합하는 function입니다.
 export default combineReducers({
     base,
-    auth
+    auth,
+    pender: penderReducer 
 });
