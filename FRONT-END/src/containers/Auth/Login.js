98ce85fca0ac1b64ad42a5dded005199efa87bb3
gorilla-Kim <00ghks22@naver.com>
2020-02-11 16:25:47 +0900

    extended Login container [create handleLocalLogin]
    로그인 버튼 클릭시 로그인
    * localStorage에 로그인 정보 저장
    * token 발급
    * 홈페이지로 이동 ('/')

--- FRONT-END/src/containers/Auth/Login.js
+++ FRONT-END/src/containers/Auth/Login.js
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
-import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink  } from 'components/Auth';
+import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink, AuthError } from 'components/Auth';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as authActions from 'redux/modules/auth';
+import * as userActions from 'redux/modules/user';
+import storage from 'lib/storage';
 
-function Login({ form, AuthActions }) {
+function Login({ form, result, error, AuthActions, UserActions, history }) {
   
   useEffect(() => {
     return () => {
       AuthActions.initializeForm('login');
     };
   }, [AuthActions])
+
+  const setError = message => {
+    AuthActions.setError({form: 'login', message});
+    return false;
+  }
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -21,11 +28,29 @@ function Login({ form, AuthActions }) {
     })
   };
 
+  const handleLocalLogin = async () => {
+    const { email, password } = form.toJS();
+
+    try {
+      await AuthActions.localLogin({email, password});
+      const loggedInfo = result.toJS();
+
+      UserActions.setLoggedInfo(loggedInfo);
+      history.push('/');
+      storage.set('loggedInfo', loggedInfo);
+    } catch (error) {
+      setError('잘못된 계정정보입니다.')
+    }
+  }
+
   return (
     <AuthContent title="로그인">
       <InputWithLabel label="아이디" name="email" placeholder="이메일" onChange={handleChange}/>
       <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password" onChange={handleChange}/>
-      <AuthButton>로그인</AuthButton>
+      {
+        error && <AuthError>{ error }</AuthError>
+      }
+      <AuthButton onClick={handleLocalLogin}>로그인</AuthButton>
       <RightAlignedLink to="/auth/register">회원가입</RightAlignedLink>
     </AuthContent>
   );
@@ -33,9 +58,12 @@ function Login({ form, AuthActions }) {
 
 export default connect(
   (state) => ({
-      form: state.auth.getIn(['login', 'form'])
+      form: state.auth.getIn(['login', 'form']),
+      error: state.auth.getIn(['login', 'error']),
+      result: state.auth.get('result')
   }),
   (dispatch) => ({
-    AuthActions: bindActionCreators(authActions, dispatch)
+    AuthActions: bindActionCreators(authActions, dispatch),
+    UserActions: bindActionCreators(userActions, dispatch)
   })
 )(Login);
