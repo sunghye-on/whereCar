3a7d31fe3c8118e185eddb7f9366919f95014a6c
gorilla-Kim <00ghks22@naver.com>
2020-02-11 15:22:48 +0900

    create user actions using redux&redux-actions

--- FRONT-END/src/redux/modules/index.js
+++ FRONT-END/src/redux/modules/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import base from './base';
 import auth from './auth';
+import user from './user';
 import { penderReducer } from 'redux-pender';
 
 // reducer들을 하나로 병합하는 function입니다.
 export default combineReducers({
     base,
     auth,
+    user,
     pender: penderReducer 
 });
