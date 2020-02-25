48b6874c9607452712b9c15def870c3bc90099bf
gorilla-Kim <00ghks22@naver.com>
2020-02-26 00:48:21 +0900

    해야할일 추가

--- FRONT-END/src/sockets/driverListSoc.js
+++ FRONT-END/src/sockets/driverListSoc.js
@@ -2,6 +2,8 @@ export default function driverListSoc(socket, SocketActions) {
   // driverList 받아오기 [API]
   SocketActions.setDriverList();
 
+  // 초기상태 만들기 with DriverActive상태도 받아오기.
+
   // Active상태 갱신하기 [Socket]
   socket.on("sendNotifDriverActive", (data) => { // data = {driver, active}
     SocketActions.setDriverStatus(data);
