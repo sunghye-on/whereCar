783c0c93b573c6a948270a65b339071574ced5a8
sunghye-on <opei71@naver.com>
2020-05-25 15:12:56 +0900

    PWA 적용전 커밋

--- BACK-END/src/api/v1.0/myList/index.js
+++ BACK-END/src/api/v1.0/myList/index.js
@@ -1,18 +1,16 @@
-const Router = require('koa-router'); 
-const myListCtrl = require('./myList.ctrl');
+const Router = require("koa-router");
+const myListCtrl = require("./myList.ctrl");
 
 const myList = new Router();
 
 /* /api/v1.0/mylist */
-myList.get('/', myListCtrl.getMyList);
+myList.get("/", myListCtrl.getMyList);
 
-myList.post('/course/active', myListCtrl.activeCourse); 
-myList.post('/course/deactive', myListCtrl.deactiveCourse); 
-myList.get('/drivers', myListCtrl.getLists); 
+myList.post("/course/active", myListCtrl.activeCourse);
+myList.post("/course/deactive", myListCtrl.deactiveCourse);
+myList.get("/drivers", myListCtrl.getLists);
 
-myList.post('/groupPushRemove', myListCtrl.groupPushRemove);
-myList.post('/coursePushRemove', myListCtrl.coursePushRemove);
-
-module.exports = myList;
+myList.post("/groupPushRemove", myListCtrl.groupPushRemove);
+myList.post("/coursePushRemove", myListCtrl.coursePushRemove);
 
 module.exports = myList;
